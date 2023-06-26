49d0c0ccd6a34ac4d77429dff764d0e3847bec3f
huangdecai <249967141@qq.com>
2023-06-26 09:55:35 +0800

    this

--- c++dll调用/LandNoFenHuaSe_erRen/gameLogic/testLogic2/testLogic2.cpp
+++ c++dll调用/LandNoFenHuaSe_erRen/gameLogic/testLogic2/testLogic2.cpp
@@ -57,18 +57,18 @@ void TestGameLogic()
 	
 	BYTE	cbHandCardData[NORMAL_COUNT] = { 0 };	// ÷…œ∆ÀøÀ
 	//BYTE    tempCard[] = { 0x1C, 0x12, 0x02, 0x38, 0x27, 0x05, 0x23, 0x03, 0x19, 0x28, 0x37, 0x26, 0x25 };
-	BYTE    tempCard[] = { 14, 1, 1, 1, 13, 12, 12, 11, 11, 9, 7, 7, 6, 5, 5, 5 };
+	BYTE    tempCard[] = { 15, 14, 2, 2, 2, 2, 12,  9,9, 8, 7, 6, 6, 6, 5 };
 	//BYTE    tempCard[] = { 2, 2, 2, 13, 10, 8, 8, 7, 7, 5 };
 
 	tagOutCardResultNew  OutCardResult;
-	BYTE cbTurnCardData[] = { 12 };// { S6, 6, 5, 5, 4, 4 };
+	BYTE cbTurnCardData[] = { 12, 12 };// { S6, 6, 5, 5, 4, 4 };
 	int cbTurnCardCount =   sizeof(cbTurnCardData);
-	BYTE cbDiscardCard[] = { 1, 1, 6, 6, 6, 5, 11, 11, 11, 7 };
-	BYTE cbDiscardCardCount =  sizeof(cbDiscardCard);
-	BYTE cbOtherDisscard[] = { 5, 5, 8, 8, 8, 6 };
+	BYTE cbDiscardCard[] = { 11, 11 };
+	BYTE cbDiscardCardCount =   sizeof(cbDiscardCard);
+	BYTE cbOtherDisscard[] = { 5, 5,12,12 };
 	BYTE cbOtherDisscardCount = sizeof(cbOtherDisscard);
-	BYTE cbCardDataEx[MAX_COUNT] = { 0, 0, 3, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	BYTE maxCard[MAX_COUNT] = { 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	BYTE cbCardDataEx[MAX_COUNT] = { 0, 0, 4, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	BYTE maxCard[MAX_COUNT] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	WORD wBankerUser = 0;
 	WORD wUndersideUser = (wBankerUser + 1) % GAME_PLAYER;
 	WORD wUpsideUser = (wUndersideUser + 1) % GAME_PLAYER;
