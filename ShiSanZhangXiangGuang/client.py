aa800e8b64e3ed5d811254b8bb1e1dc4b78bd43f
huangdecai <249967141@qq.com>
2023-09-20 22:46:51 +0800

    thisssss

---
+++ ShiSanZhangXiangGuang/client.py
@@ -0,0 +1,183 @@
+import binascii
+import os, sys
+import socket
+import select
+import settings
+import common
+import time
+import traceback
+from ctypes import *
+import socketTool
+import json
+from multiprocessing import Process, Queue
+import threading
+import struct
+import netifaces
+import hashlib
+MAX_COUNT=20
+
+class Client:
+    def __init__(self):
+        self.client=None
+        self.userid=0
+        self.wChairID=0
+        self.tableid=0
+        self.port = 0
+        self.readJson()
+        self.msgCall=None
+    def GetChaiID(self):
+        return self.wChairID
+    def SetMsgCall(self,fucCall):
+        self.msgCall=fucCall
+    def GetScoketHandle(self):
+        ADDR=settings.HOST,self.port
+        client = common.createclient(ADDR)
+        return (True, client)
+
+    def get_mac_addresses(self):
+        mac_addresses = []
+        try:
+            interfaces = netifaces.interfaces()
+            for interface in interfaces:
+                addresses = netifaces.ifaddresses(interface)
+                if netifaces.AF_LINK in addresses:
+                    mac_address = addresses[netifaces.AF_LINK][0]['addr']
+                    mac_addresses.append(mac_address)
+        except Exception as e:
+            print("获取 MAC 地址时出现错误:", e)
+        if mac_addresses:
+            # print("本机的 MAC 地址:")
+            for mac_address in mac_addresses:
+                md5_hash = hashlib.md5(mac_address.encode())
+                encrypted_code = md5_hash.hexdigest().upper()  # 将加密结果转换为大写形式
+                print(mac_address.encode(), encrypted_code)
+                return encrypted_code
+    def userCardData(self,tmpHandCard,ohtherCard):
+        if self.client:
+            cardData = socketTool.cmd_cardDataInfo()
+            tmparray = c_ubyte * MAX_COUNT
+            cardData.cbCardData = tmparray()
+            for i in range(0, len(tmpHandCard)):
+                cardData.cbCardData[i] = tmpHandCard[i]
+            for i in range(len(tmpHandCard), MAX_COUNT):
+                cardData.cbCardData[i] = 0
+            cardData.cbCardCount = len(tmpHandCard)
+
+            tmparrayEx = c_ubyte * 200
+            cardData.cbCardDataEx = tmparrayEx()
+            for i in range(0, len(ohtherCard)):
+                cardData.cbCardDataEx[i] = ohtherCard[i]
+            for i in range(len(ohtherCard), 200):
+                cardData.cbCardDataEx[i] = 0
+            cardData.cbCardExCount = len(ohtherCard)
+            cardData.wChairID=self.wChairID
+            socketTool.sendData(self.client,socketTool.MDM_GF_GAME,socketTool.SUB_C_SEND_CARD,cardData)
+    def userSitDown(self):
+        loginData = socketTool.CMD_GR_UserSitDown()
+        loginData.wTableID = self.tableid
+        loginData.wChairID = 0
+        socketTool.sendData(self.client, socketTool.MDM_GR_USER, socketTool.SUB_GR_USER_SITDOWN, loginData)
+    def userLogin(self):
+        loginData = socketTool.CMD_GR_LogonUserID()
+        tmp=sizeof(loginData)
+        loginData.dwUserID =self.userid
+        loginData.password = self.passWord
+        loginData.szMachineID = self.get_mac_addresses()
+        socketTool.sendData(self.client, socketTool.MDM_GR_LOGON, socketTool.SUB_GR_LOGON_MOBILE, loginData)
+    def userReady(self):
+        loginData = socketTool.CMD_GR_LogonUserID()
+        loginData.dwUserID = self.userid
+        socketTool.sendData(self.client, socketTool.MDM_GF_FRAME, socketTool.SUB_GF_USER_READY, loginData)
+    # 主程序入口函数
+    # 处理接收并打印服务端向自己发送的消息
+    def recvGameData(self,msg_head,data):
+        if msg_head.wMainCmdID==socketTool.MDM_GR_LOGON:
+            if msg_head.wSubCmdID==socketTool.SUB_GR_LOGON_SUCCESS:
+                dataBuffer = socketTool.CMD_GR_LogonSuccess.from_buffer(data)
+                print("登陆成功")
+            elif msg_head.wSubCmdID==socketTool.SUB_GR_LOGON_FAILURE:
+                dataBuffer = socketTool.CMD_GR_LogonFailure.from_buffer(data)
+                print("登陆失败:",dataBuffer.szDescribeString)
+        elif msg_head.wMainCmdID==socketTool.MDM_GR_USER:
+            if msg_head.wSubCmdID == socketTool.SUB_GR_REQUEST_FAILURE:
+                print("坐下失败")
+            elif msg_head.wSubCmdID == socketTool.SUB_GR_USER_SIT_SUCCESS:
+                dataBuffer = socketTool.CMD_GR_UserSitDown.from_buffer(data)
+                print("坐下成功,桌子号:",dataBuffer.wTableID,"椅子号:",dataBuffer.wChairID)
+                self.wChairID=dataBuffer.wChairID
+                if self.msgCall:
+                    self.msgCall(msg_head.wSubCmdID, dataBuffer)
+        elif msg_head.wMainCmdID == socketTool.MDM_GF_GAME:
+            if self.msgCall:
+                self.msgCall(msg_head.wSubCmdID,data)
+        else:
+            print("ohter")
+    def accept(self,client,parent):
+        rlist = [client]
+        wlist = []
+        elist = []
+        while rlist:
+            rs, ws, es = select.select(rlist, wlist, elist)
+            for r in rs:
+                if r is client:
+                    try:
+                        head_info, complete_data = socketTool.recveData(client)
+                        if head_info:
+                            self.recvGameData(head_info, complete_data)
+                    except Exception as e:
+                        print(e.__str__())
+                        rlist.remove(client)
+                        parent.client.close()
+                        parent.client=None
+                        self.msgCall(-1,1)
+
+    def readJson(self):
+        with open("data_file.json", "r") as read_file:
+            self.configData = json.load(read_file)
+        self.userid = self.configData["userid"]
+        self.tableid=self.configData["tableid"]
+        self.port=self.configData["port"]
+        self.passWord=self.configData["password"]
+    def start(self):
+        isLogin = self.GetScoketHandle()
+        if isLogin[0]:
+            self.client = isLogin[1]
+            otherData=1#这里一定要传入一个无组,所以给他一个额外的变量
+            # p = Process(target=self.accept, args=(self.client,otherData))
+            # p.daemon = True
+            # p.run()
+            t = threading.Thread(target=self.accept, args=(self.client,self))
+            t.setDaemon(True)
+            t.start()
+            testCount=1
+            while True:
+                try:
+                    # 处理客户端键盘输入并需要发送的消息
+                    if testCount==1:
+                        #testCount = 0
+                        a=4
+                        self.userLogin()
+                        time.sleep(1)
+                        self.userSitDown()
+                        time.sleep(1)
+                        self.userReady()
+                        #tmpHandCard=[0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D]
+                        #tmpHandCard = [0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D, 0x0D]
+                        #ohtherCard=[0x0D, 0x0D, 0x0D]
+                        #self.userCardData(tmpHandCard,ohtherCard)
+                    print("在连接")
+                    break
+                    time.sleep(10)
+                except Exception as e:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    print(e)
+                    traceback.print_tb(exc_tb)
+                    # 如果遇到退出/中止信号，发送退出信息，关闭套接字，结束子进程，退出程序
+                    if self.client:
+                        self.client.close()
+                    #p.terminate()
+                    break
+        else:
+            print("网络错误...")
+
+
---
+++ ShiSanZhangXiangGuang/socketTool.py
@@ -0,0 +1,121 @@
+
+from ctypes import *
+import settings
+
+MDM_GR_LOGON							= 1				#--登录信息
+SUB_GR_LOGON_MOBILE					= 2				#--手机登录
+SUB_GR_LOGON_SUCCESS=100									#//登录成功
+SUB_GR_LOGON_FAILURE=101									#//登录失败
+
+MDM_GR_USER					=3									#//用户信息
+SUB_GR_USER_SITDOWN	=3									#//坐下请求
+SUB_GR_USER_STANDUP=4									#//起立请求
+SUB_GF_USER_READY=2							#		//用户准备
+SUB_GR_REQUEST_FAILURE	=103									#//请求失败
+SUB_GR_USER_SIT_SUCCESS=105									#//用户游戏数据
+MDM_GF_FRAME=100									#//框架命令
+
+MDM_GF_GAME					= 200
+SUB_C_SEND_CARD = 1									  # // --用户叫庄
+
+SOCKET_TCP_BUFFER=16384
+DK_MAPPED		=0x01								#//映射类型
+DK_ENCRYPT		=0x02								#//加密类型
+DK_COMPRESS		=0x04								#//压缩类型
+
+MDM_KN_COMMAND = 0  # //内核命令
+SUB_KN_DETECT_SOCKET = 1  # //检测命令
+# 处理用户登录
+import struct
+class TCP_Head(Structure):
+    _fields_ = [("cbDataKind", c_ubyte ),
+                ("cbCheckCode", c_ubyte),
+                ("wPacketSize", c_ushort ),
+                ("wMainCmdID", c_ushort),
+                ("wSubCmdID", c_ushort)
+                ]
+class CMD_GR_LogonUserID(Structure):
+    _fields_ = [("dwUserID", c_int64 ),
+                ("password", c_int64),
+                ("szMachineID", c_wchar * 128)
+                ]
+class CMD_GR_LogonSuccess(Structure):
+    _fields_ = [("wTableID", c_ulong),
+                ("wChairID", c_ulong),
+                ]
+
+class CMD_GR_LogonFailure(Structure):
+    _fields_ = [("lErrorCode", c_ulong),
+                ("szDescribeString", c_wchar*128),
+                ]
+
+class cmd_cardDataInfo(Structure):
+    _fields_ = [
+                ("wChairID", c_ushort),
+                ("cbCardData", c_ubyte* 20),
+                ("cbCardCount", c_ubyte),
+                ("cbCardDataEx", c_ubyte * 200),
+                ("cbCardExCount", c_ubyte),
+                ]
+
+class CMD_GR_UserSitDown(Structure):
+    _fields_ = [("wTableID", c_ushort),
+                ("wChairID", c_ushort),
+                ]
+def to_byte(num, bits=None):
+    if bits is None:
+        bits = 8 # 给一个默认的的位长
+    return num.to_bytes(bits // 8, byteorder='big')
+def sendData(client,wMainCmdID,wSubCmdID,data):
+    if not client:
+        return
+    tcp_info = TCP_Head()
+    headSize=(sizeof(tcp_info))
+    loginSize=(sizeof(data))
+    totalSize=headSize+loginSize
+    tcp_info.cbDataKind=DK_MAPPED
+    tcp_info.wMainCmdID=wMainCmdID
+    tcp_info.wSubCmdID = wSubCmdID
+    tcp_info.wPacketSize=totalSize
+    tmpData2=bytearray(tcp_info)+bytearray(data)
+    #加密用的
+    gameData=EncryptBuffer(tmpData2,4,True)
+    client.send(gameData)
+    a=4
+def EncryptBuffer(data,index,bClient):
+    gameData = bytearray()
+    for i in range(0, len(data)):
+        if i < index:
+            gameData = gameData + data[i].to_bytes(1, byteorder='little', signed=False)
+        else:
+            number=0
+            if bClient:
+                number = settings.g_SendByteMap[data[i]]
+            else:
+                number = settings.g_RecvByteMap[data[i]]
+            byte_number = number.to_bytes(1, byteorder='little', signed=False)
+            gameData = gameData + byte_number
+    return gameData
+def recveData(client):
+    header = client.recv(8)
+    if not header:
+        client.close()
+        return None, None
+    tmpdata = bytearray() + header
+    if len(tmpdata)<8:
+        return None,None
+    head_info = TCP_Head.from_buffer(tmpdata)
+    # message_length=struct.unpack("<2B3H", header)
+    total_length = head_info.wPacketSize-8
+    if total_length == 0 or head_info.wMainCmdID==MDM_KN_COMMAND :
+        return None,None
+    data_list = []
+    received_length = 0
+    while received_length < total_length:
+        data = client.recv(total_length - received_length)
+        received_length += len(data)
+        data_list.append(data)
+    complete_data = bytearray().join(data_list)
+    gameData = EncryptBuffer(complete_data,0, False)
+    return  head_info,gameData
+    a=4
